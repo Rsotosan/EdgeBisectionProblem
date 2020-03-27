9728f19852d6f50b3b25c1c5934b1663158c2a42
rsotosan <sotosanchezraul@gmail.com>
2020-03-27 18:55:24 +0100

    Improve reevaluate method in GRASP

--- src/algorithm/Grasp.java
+++ src/algorithm/Grasp.java
@@ -30,8 +30,6 @@ public class Grasp implements Algorithm{
         for (int i = 0; i<graph.order(); i++){
             candidates.add(new Candidate(i));
         }
-        //First cost evaluate of candidates
-        evaluateCost(vertexList1, candidates);
         //Iteration for a solution
         while(vertexList1.size() < graph.order() / 2){
             minCost = candidates.getLast().getCost();
@@ -39,6 +37,7 @@ public class Grasp implements Algorithm{
             th = maxCost - alpha * (maxCost - minCost);
 
             //improvable?
+
             int rcl = 0;
             while(rcl < candidates.size() && candidates.get(rcl).getCost() >= th){
                 rcl++;
@@ -50,7 +49,7 @@ public class Grasp implements Algorithm{
             vertexList1.add(randomCandidate.getId());
 
             //Reevaluate cost of partial candidate list
-            evaluateCost(vertexList1, candidates);
+            reevaluateCost(vertexList1, candidates,randomCandidate.getId());
         }
         for(int i = 0; i<graph.order(); i++){
             if(!vertexList1.contains(i)){
@@ -61,16 +60,24 @@ public class Grasp implements Algorithm{
         return solution;
     }
 
+    private void reevaluateCost(HashSet<Integer> partialSolution, LinkedList<Candidate> candidates, int newV){
+        for (Candidate c: candidates) {
+            if(graph.adjacent(c.getId(), newV)) c.setCost(c.getCost()+1);
+            if(graph.adjacent(newV, c.getId())) c.setCost(c.getCost()+1);
+        }
+        candidates.sort((Candidate c1, Candidate c2) -> c2.getCost() - c1.getCost());
+    }
+
     private void evaluateCost(HashSet<Integer> partialSolution, LinkedList<Candidate> candidates){
         for (Candidate c: candidates) {
             int cost = 0;
             ArrayList<Integer> neighbors = graph.neighbors(c.getId());
-            /**
+            /*
             for (Integer id: partialSolution){
                 if(graph.adjacent(c.getId(), id)) cost ++;
                 if(graph.adjacent(id, c.getId())) cost ++;
             }
-             */
+            */
             for (int neighbor: neighbors){
                 if(partialSolution.contains(neighbor)) cost ++;
             }
